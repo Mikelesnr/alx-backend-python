78257db438fc5a34d628298a048837cbc82f4a1e
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-07 19:53:07 +0200

    log queries updatre

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
+
 import sqlite3
 import functools
-import logging
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+from datetime import datetime
 
 #### decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0]  # Assuming the first argument is the SQL query
-        logging.info(f"Executing query: {query}")
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"[{timestamp}] Executing query: {query}")
         result = func(*args, **kwargs)
-        logging.info("Query executed successfully")
+        print(f"[{timestamp}] Query executed successfully")
         return result
     return wrapper
 
