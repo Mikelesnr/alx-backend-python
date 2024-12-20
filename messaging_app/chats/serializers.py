0a959abe92eb3654c8fc9f681bdbe47b0d3e14a7
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-20 20:30:21 +0200

    Serializers validation added

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -13,10 +13,19 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
+    def validate_message_body(self, value):
+        if len(value) < 1:
+            raise serializers.ValidationError("Message body cannot be empty.")
+        return value
+
 class ConversationSerializer(serializers.ModelSerializer):
     participants = CustomUserSerializer(many=True, read_only=True)
-    messages = MessageSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'messages', 'created_at']
+
+    def get_messages(self, obj):
+        messages = Message.objects.filter(conversation=obj)
+        return MessageSerializer(messages, many=True).data
