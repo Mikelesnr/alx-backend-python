afe55141a0a84b5be289b08797ee5bceb4e56f70
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-20 20:50:46 +0200

    urls adjustment

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,17 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from chats.views import ConversationViewSet, MessageViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
-
-urlpatterns += [
-    path('', include(router.urls)),
+    path('chats/', include('chats.urls')),
 ]
