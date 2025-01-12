4a12c67ae634ee33b9c6ab480fc6f86ed0bfcbca
Mikelesnr <mwanza.n.m@gmail.com>
2025-01-12 21:50:29 +0200

    create message added

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Message
+from .forms import MessageForm
 
 def conversation_view(request, conversation_id):
     messages = Message.objects.filter(conversation_id=conversation_id).select_related('sender', 'receiver').prefetch_related('replies')
@@ -12,3 +13,16 @@ def delete_user(request):
     user = request.user
     user.delete()
     return redirect('home')  # Redirect to the home page or any other page after deletion
+
+@login_required
+def create_message(request):
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.save()
+            return redirect('conversation_view', conversation_id=message.conversation_id)
+    else:
+        form = MessageForm()
+    return render(request, 'messaging/create_message.html', {'form': form})
