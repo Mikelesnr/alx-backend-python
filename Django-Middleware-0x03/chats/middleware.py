938178b579c103a6687d24113db72e2d2f5cf2f9
Mikelesnr <mwanza.n.m@gmail.com>
2025-01-14 22:56:20 +0200

    RestrictedAccessByTimemiddleware added

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,18 @@
 from datetime import datetime
 from django.http import HttpResponseForbidden
 
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        log_message = f"{datetime.datetime.now()} - User: {user} - Path: {request.path}"
+        with open('requests.log', 'a') as log_file:
+            log_file.write(log_message + '\n')
+        response = self.get_response(request)
+        return response
+
 class RestrictAccessByTimeMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
