ffb1c1da286e148496149c1a25a6a71a3b319074
Mikelesnr <mwanza.n.m@gmail.com>
2025-01-14 23:18:17 +0200

    dttime fix

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -8,7 +8,7 @@ class RequestLoggingMiddleware:
 
     def __call__(self, request):
         user = request.user if request.user.is_authenticated else 'Anonymous'
-        log_message = f"{datetime.datetime.now()} - User: {user} - Path: {request.path}"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
         with open('requests.log', 'a') as log_file:
             log_file.write(log_message + '\n')
         response = self.get_response(request)
